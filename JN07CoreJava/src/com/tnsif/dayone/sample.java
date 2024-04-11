ae6141881ab59e7daa2083983956355da0266286
pawar <pawar@LAPTOP-9I1BCPOU>
2024-04-12 03:26:40 +0530

    initial commit

---
+++ JN07CoreJava/src/com/tnsif/dayone/sample.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("hello abhishek");
+		// TODO Auto-generated method stub
+
+	}
+
+}
